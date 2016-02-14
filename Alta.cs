41dd26a29bbb4b8ed6bfff929ff09302f249d1c4
Genaro De La Torre <genaro166@hotmail.com>
2016-02-13 16:29:40 -0800

    Se agregaron clases

---
+++ Alta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElTrompo
+{
+    class Alta
+    {
+        //dar de alta clientes
+        public static void Cliente()
+        {
+            
+        }
+
+        //dar de alta agenda
+        public static void Agenda()
+        {
+
+        }
+
+    }
+}
