06eaf9ea1c7635ed5306a5a92a6ad9a623a34ec8
Jin Oh <javieroh@hotmail.com>
2016-02-14 15:52:47 -0800

    commit
    commit

--- Alta.cs
+++ Alta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace ElTrompo
         public void Cliente()
         {
             //sql query
+            SqlConnection connection = new SqlConnection();
         }
 
         public void Agenda()
