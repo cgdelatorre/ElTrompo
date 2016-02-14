82d8b66bbecccb53e1b54a795bc9b736c1ac4b77
Jin Oh <javieroh@hotmail.com>
2016-02-13 16:03:56 -0800

    Cambios
    Puse un label

--- obj/Debug/MainWindow.g.i.cs
+++ obj/Debug/MainWindow.g.i.cs
@@ -40,6 +40,14 @@ namespace ElTrompo {
     /// </summary>
     public partial class MainWindow : System.Windows.Window, System.Windows.Markup.IComponentConnector {
         
+        
+        #line 10 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.Label label;
+        
+        #line default
+        #line hidden
+        
         private bool _contentLoaded;
         
         /// <summary>
@@ -68,6 +76,12 @@ namespace ElTrompo {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.label = ((System.Windows.Controls.Label)(target));
+            return;
+            }
             this._contentLoaded = true;
         }
     }
